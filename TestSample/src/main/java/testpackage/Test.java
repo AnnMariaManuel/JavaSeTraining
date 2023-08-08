c3446f4a90a4044bef5e28a296ecea66ecada33e
Tony joseph <Tony joseph@LAPTOP-8OG7RLSC>
2023-08-08 08:32:42 +0530

    new Test maven project

--- TestSample/src/main/java/testpackage/Test.java
+++ TestSample/src/main/java/testpackage/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) 
 	
 	{
-		System.out.println("This is a sample classs in maven project ");
+		System.out.println("This is a sample classs in maven project modified now ");
 
 	}
 

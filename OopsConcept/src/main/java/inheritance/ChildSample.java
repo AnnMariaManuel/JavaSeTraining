785cb8436cc5033d9b64c0f3db0a31f6edc4b41d
Tony joseph <Tony joseph@192.168.1.36>
2023-08-09 08:00:57 +0530

    minor chnages

--- OopsConcept/src/main/java/inheritance/ChildSample.java
+++ OopsConcept/src/main/java/inheritance/ChildSample.java
@@ -5,7 +5,7 @@ public class ChildSample extends ParentSample
 	
 	public void print()
 	{
-		
+		System.out.println(a);
 		System.out.println("This is a child class modified now");
 	}
 
--- OopsConcept/src/main/java/inheritance/ParentSample.java
+++ OopsConcept/src/main/java/inheritance/ParentSample.java
@@ -3,7 +3,7 @@ package inheritance;
 public class ParentSample 
 
 {
-	
+	int a=10;
 	public void display()
 	{
 		System.out.println("This is a parent class");

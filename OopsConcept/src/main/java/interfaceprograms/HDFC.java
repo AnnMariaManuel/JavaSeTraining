0cff9bcd3dfea173f4820d2e27179d2ce1f112f3
Tony joseph <Tony joseph@LAPTOP-8OG7RLSC>
2023-08-22 16:46:32 +0530

    chnages

--- OopsConcept/src/main/java/interfaceprograms/HDFC.java
+++ OopsConcept/src/main/java/interfaceprograms/HDFC.java
@@ -1,15 +1,15 @@
 package interfaceprograms;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class HDFC implements RBI
 {
 
-	static int deposit_amt;
-	static int deposit_tenure;
+	//static int deposit_amt;
+	//static int deposit_tenure;
 
 	@Override
-	public void recurringDeposit() 
+	public void recurringDeposit(int deposit_amt,int deposit_tenure ) 
 	{
 		float maturity_amt=  deposit_amt + ((deposit_amt*interest_rate*deposit_tenure)/100);
 		System.out.println("Maturity amount is :" +maturity_amt);
@@ -19,15 +19,15 @@ public class HDFC implements RBI
 public static void main(String[] args) 
 	
 	{
-		Scanner sc=new Scanner(System.in);
+		/*Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the deposit Amount :");
 		deposit_amt= sc.nextInt();
 		System.out.println("Enter deposit tenure:");
 		deposit_tenure=sc.nextInt();
-		sc.close();
+		sc.close();*/
 		
 		HDFC h= new HDFC();
-		h.recurringDeposit();
+		h.recurringDeposit(10000,3);
 		
 
 	}
--- OopsConcept/src/main/java/interfaceprograms/RBI.java
+++ OopsConcept/src/main/java/interfaceprograms/RBI.java
@@ -5,7 +5,7 @@ public interface RBI
 {
 	
 	float interest_rate= 6.5f;
-	void recurringDeposit();
+	void recurringDeposit(int deposit_amt, int deposit_tenure);
 	
 
 }
